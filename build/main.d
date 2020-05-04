c0a67c1f53dde395678970fcc48bf45911633349
Vincent <suncraft.mc89@gmail.com>
2020-05-04 19:12:30 +0200

    Add files via upload

---
+++ build/main.d
@@ -0,0 +1,40 @@
+main.o: /home/vincent/Bureau/GamepadTester/src/main.c \
+ /opt/devkitpro/wut/include/coreinit/thread.h \
+ /opt/devkitpro/wut/include/wut.h \
+ /opt/devkitpro/wut/include/wut_structsize.h \
+ /opt/devkitpro/wut/include/wut_types.h \
+ /opt/devkitpro/wut/include/wut_rplwrap.h \
+ /opt/devkitpro/wut/include/coreinit/context.h \
+ /opt/devkitpro/wut/include/coreinit/time.h \
+ /opt/devkitpro/wut/include/coreinit/systeminfo.h \
+ /opt/devkitpro/wut/include/coreinit/threadqueue.h \
+ /opt/devkitpro/wut/include/vpad/input.h \
+ /opt/devkitpro/wut/include/whb/proc.h \
+ /opt/devkitpro/wut/include/whb/log.h \
+ /opt/devkitpro/wut/include/whb/log_console.h
+
+/opt/devkitpro/wut/include/coreinit/thread.h:
+
+/opt/devkitpro/wut/include/wut.h:
+
+/opt/devkitpro/wut/include/wut_structsize.h:
+
+/opt/devkitpro/wut/include/wut_types.h:
+
+/opt/devkitpro/wut/include/wut_rplwrap.h:
+
+/opt/devkitpro/wut/include/coreinit/context.h:
+
+/opt/devkitpro/wut/include/coreinit/time.h:
+
+/opt/devkitpro/wut/include/coreinit/systeminfo.h:
+
+/opt/devkitpro/wut/include/coreinit/threadqueue.h:
+
+/opt/devkitpro/wut/include/vpad/input.h:
+
+/opt/devkitpro/wut/include/whb/proc.h:
+
+/opt/devkitpro/wut/include/whb/log.h:
+
+/opt/devkitpro/wut/include/whb/log_console.h:
